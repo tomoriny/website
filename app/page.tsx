9abc5acb01a5d18493030a24649071e005bf016f
Hydration <168127639+tomoriny@users.noreply.github.com>
2024-12-15 15:35:41 +0800

    chore: sync

--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,9 @@ const html = `
 <blockquote>
 <p>Hey 👋 Nice to meet you here.</p>
 </blockquote>
-<p>My nickname is <strong>Hydration</strong>, I come from Guangdong, China.</p>
+
 <p>I am a student as well as a open sourceror, a Web developer and a UI/UX designer. And I am studying in Guangdong Experimental High School currently.</p>
-<h2>I am</h2>
-<ul>
-<li>Core member team of <a href="https://github.com/EdgelessPE">Edgeless PE</a></li>
-<li>Co-founder of <a href="https://github.com/cladonia-cn">Cladonia LLC.</a></li>
-</ul>
-<h2>I am currently focusing on</h2>
-<ul>
-<li>Modern Web Technologies. JavaScript, Vue, Vite and TypeScript.</li>
-</ul>
-<h2>Contact me</h2>
-<ul>
-<li>GitHub <a href="https://github.com/hydrati">@hydrati</a></li>
-<li>bilibili <a href="https://space.bilibili.com/98955693">@水合Hydration</a></li>
-<li>Email <a href="mailto:hy@cladonia.team">hy@cladonia.team</a></li>
-<li>Twitter <a href="https://twitter.com/_hydrati">@_hydrati</a></li>
+
 </ul>
 
 `
